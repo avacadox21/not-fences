56e7d344077959f6e8c3e51d2c07c92ef70a0086
Rohit Chowdhary <avacadox21@gmail.com>
2022-01-09 22:20:40 +0530

    lel

---
+++ Model/FenceInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoFences.Model
+{
+    public class FenceInfo
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public bool Locked { get; set; }
+
+        public bool CanMinify { get; set; }
+
+        public int TitleHeight { get; set; } = 35;
+
+        public List<string> Files { get; set; } = new List<string>();
+
+        public FenceInfo()
+        {
+
+        }
+
+        public FenceInfo(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using NoFences.Model;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NoFences
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var mutex = new Mutex(true, "No_fences", out var createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+
+                    FenceManager.Instance.LoadFences();
+                    if (Application.OpenForms.Count == 0)
+                        FenceManager.Instance.CreateFence("First fence");
+
+                    Application.Run();
+                }
+            }
+        }
+
+    }
+}
